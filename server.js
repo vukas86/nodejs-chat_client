3ce45d805e73f6eeade49ffc319b77f76fa10204
“vukas86” <vukasbit@gmail.com>
2023-06-13 23:06:32 +0200

    Implement data sending to MongoDB database for storage and retrieval

--- server.js
+++ server.js
@@ -3,28 +3,43 @@ const bodyParser = require("body-parser");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const mongoose = require("mongoose");
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-let messages = [
-  { name: "Tim", message: "Hi" },
-  { name: "Jane", message: "Hello" },
-];
+const dburl = "mongodb+srv://user:user@cluster0.6vi8s2m.mongodb.net/";
+
+let Message = mongoose.model("Message", { name: String, message: String });
 
 app.get("/messages", (req, res) => {
-  res.send(messages);
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  });
 });
 
 app.post("/messages", (req, res) => {
-  console.log(req.body);
-  messages.push(req.body);
-  res.sendStatus(200);
+  let message = new Message(req.body);
+
+  message.save((err) => {
+    if (err) sendStatus(500);
+
+    io.emit("message", req.body);
+    res.sendStatus(200);
+  });
 });
 
 io.on("connection", (socket) => {
   console.log("an user connected");
+});
+
+// const connectToMongo = async () => {
+//   mongoose.connect(dburl, await console.log("connected"));
+// };
+
+mongoose.connect(dburl, (err) => {
+  console.log("mongo db cibb", err);
 });
 
 const server = http.listen(3000, () => {
